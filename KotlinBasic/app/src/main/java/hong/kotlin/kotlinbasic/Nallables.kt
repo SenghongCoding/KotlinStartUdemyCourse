beb5ea4b19865303510b7048351f6757c81714f9
hangsenghong <senghonghang@gmail.com>
2022-08-24 14:44:31 +0700

    add nullable file

--- KotlinBasic/app/src/main/java/hong/kotlin/kotlinbasic/Nallables.kt
+++ KotlinBasic/app/src/main/java/hong/kotlin/kotlinbasic/Nallables.kt
@@ -1,5 +1,27 @@
 package hong.kotlin.kotlinbasic
 
 fun main() {
+    var name:String = "senghong"
+    name = "Khmer"
+    var NullableName:String? ="Testing"
+//    NullableName = null
+
+    var myName = NullableName ?: "Testing"
+//    print("$myName")
+   println( NullableName!!.toUpperCase())
+
+    var len = name.length
+
+    var len2 = NullableName?.length
+//    NullableName?.let { println(it.length) }
+//    println(NullableName?.toLowerCase())
+//     or
+    /*if (NullableName !=null){
+        var len2 = NullableName.length
+    }else{
+        null
+    }*/
+
+//    var wifeAge : String ? = user?wife?age? :0
 
 }
